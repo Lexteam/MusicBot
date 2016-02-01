2c298361181ec32909eb576a722f40c76e1cd3a1
Jamie Mansfield <dev@jamierocks.uk>
2016-02-01 18:18:15 +0000

    Almost complete play command

--- index.js
+++ index.js
@@ -12,7 +12,25 @@ bot.on('message', function (message) {
         if (args[0] === '!help') {
             bot.sendMessage(message.channel, 'A help page will be availiable soon.');
         } else if (args[0] === '!summon') {
-            bot.joinVoiceChannel(message.author.voiceChannel);
+            if (message.author.voiceChannel == null) {
+                bot.sendMessage(message.channel, 'That user is not in a voice channel!');
+            } else {
+                bot.joinVoiceChannel(message.author.voiceChannel);
+                bot.sendMessage(message.channel, 'Joined voice channel successfully.');
+            }
+        } else if (args[0] === '!play') {
+            if (args[1] == null) {
+                bot.sendMessage(message.channel, 'Usage: !play <url>');
+                return;
+            }
+            
+            var video = youtubedl(args[1]);
+            video.on('info', function(info) {
+                if (!fs.exists("./audio_cache/" + info.id + ".mp3")) {
+                    video.pipe(fs.createWriteStream("./audio_cache/" + info.id + ".mp3"));
+                }
+                //bot.voiceConnection.playFile("./audio_cache/" + info.id + ".mp3");
+            });
         }
     }
 });
