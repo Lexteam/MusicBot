24ef15486711b30bf356f19686041e78d47ccd79
Jamie Mansfield <dev@jamierocks.uk>
2016-02-01 16:41:44 +0000

    Switch to discord.js

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 var fs = require('fs');
 var config = require('./config.json');
-var DiscordClient = require('discord.io');
+var Discord = require('discord.js');
 var youtubedl = require('youtube-dl');
 
-var bot = new DiscordClient(config);
+var bot = new Discord.Client();
 
-bot.on('message', function(user, userID, channelID, message, rawEvent) {
-    if (message[0] === '!') {
-        // it's a command handle it.
-        // commands: help, play
+bot.on('message', function (message) {
+    if (message.content.indexOf('!') === 0) {
+        var args = message.content.split(' ');
         
-        var commandSplit = message.split(' ');
-        if (commandSplit[0] === '!help') {
-            bot.sendMessage({
-                to: channelID,
-                message: 'A help page will be availiable soon.'
-            });
-        } else if (commandSplit[0] == '!play') {
-            if (commandSplit[1] === null || commandSplit[1] === '') {
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Usage: !play <youtube_url>'
-                });
-            } else {
-                var video = youtubedl(commandSplit[1]);
-                video.pipe(fs.createWriteStream('audio_cache/' + commandSplit[1] + '.mp4'));
-                
-                bot.getAudioContext(channelID, function(stream) {
-                    stream.playAudioFile('audio_cache/' + commandSplit[1] + '.mp4');
-                });
-            }
+        if (args[0] === '!help') {
+            bot.sendMessage(message.channel, 'A help page will be availiable soon.');
+        } else if (args[0] === '!summon') {
+            bot.joinVoiceChannel(message.author.voiceChannel);
         }
     }
-}
+});
+
+bot.login(config.email, config.password);
